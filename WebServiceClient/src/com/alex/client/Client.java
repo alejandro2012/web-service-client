e51c44d4b6c1ba4b6225a5da34c9447aec94df4f
alejandro2012 <alejamdro.arzate.2012@gmail.com>
2014-07-16 16:43:35 -0600

    fixed client

--- WebServiceClient/src/com/alex/client/Client.java
+++ WebServiceClient/src/com/alex/client/Client.java
@@ -2,9 +2,12 @@ package com.alex.client;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+
 import javax.xml.namespace.QName;
 import javax.xml.ws.Service;
-import com.alex.endPointInterface.*;
+
+import com.alex.endpointimplementation.AddServiceServiceName;
+import com.alex.endpointimplementation.ServiceInterface01;
 
 public class Client {
 	
@@ -15,6 +18,12 @@ public class Client {
 		ServiceInterface01 i = service.getPort(ServiceInterface01.class);		
 		System.out.println(i.makeAdd(1, 3));
 		System.out.println(i.makeSubstract(1, 1));
+		
+		AddServiceServiceName service02 = new AddServiceServiceName();
+		ServiceInterface01 calc_service = service02.getAddServicePortName();
+		System.out.println( calc_service.makeAdd(1, 3) );
+		System.out.println( calc_service.makeSubstract(10, 1) );
+		
 	}
 
 }
